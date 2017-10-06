7e630a397534717b38da78bb5afc25bb1c4a948c
zhangpeng <legend123>
2017-10-06 20:32:49 +0800

    1.1.添加可以选择显示任意边框的TextView，和TextView的背景颜色不冲突，更多的适用于表格

---
+++ VettelOpenSource/app/src/main/java/ll/opensource/view/BorderTextView.java
@@ -0,0 +1,87 @@
+package ll.opensource.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import ll.opensource.R;
+
+/**
+ * Created by ZhangPeng on 9-22-0022.
+ */
+
+@SuppressLint("AppCompatCustomView")
+//自定义TextView显示任意边框
+public class BorderTextView extends TextView {
+
+    private boolean leftBorder;
+    private boolean topBorder;
+    private boolean rightBorder;
+    private boolean bottomBorder;
+
+    public BorderTextView(Context context) {
+        this(context, null);
+    }
+
+    public BorderTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BorderTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.view_border, defStyleAttr, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.view_border_left_border:
+                    leftBorder = a.getBoolean(attr, false);
+                    break;
+                case R.styleable.view_border_top_border:
+                    topBorder = a.getBoolean(attr, false);
+                    break;
+                case R.styleable.view_border_right_border:
+                    rightBorder = a.getBoolean(attr, false);
+                    break;
+                case R.styleable.view_border_bottom_border:
+                    bottomBorder = a.getBoolean(attr, false);
+                    break;
+
+            }
+        }
+        a.recycle();
+    }
+
+    //边框宽度
+    private int stroke_width = 1;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        //  将边框设为黑色
+        paint.setColor(android.graphics.Color.BLACK);
+        //  画TextView的4个边
+        //上边
+        if (topBorder) {
+            canvas.drawLine(0, 0, this.getWidth() - stroke_width, 0, paint);
+        }
+        //左边
+        if (leftBorder) {
+            canvas.drawLine(0, 0, 0, this.getHeight() - stroke_width, paint);
+        }
+        //右边
+        if (rightBorder) {
+            canvas.drawLine(this.getWidth() - stroke_width, 0, this.getWidth() - stroke_width, this.getHeight() - stroke_width, paint);
+        }
+        //下边
+        if (bottomBorder) {
+            canvas.drawLine(0, this.getHeight() - stroke_width, this.getWidth() - stroke_width, this.getHeight() - stroke_width, paint);
+        }
+        super.onDraw(canvas);
+    }
+}
